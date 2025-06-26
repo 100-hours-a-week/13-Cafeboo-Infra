b67da9c5378d0de8d201af29c5a44bb6db46a8d0
himi-i <pphm12241224@gmail.com>
2025-06-26 10:02:49 +0900

    fix : ai 모델 수정

--- v2-migration/modules/ai-models/outputs.tf
+++
@@ -1,7 +0,0 @@
-output "embedding_model_url" {
-  value = "https://storage.googleapis.com/${google_storage_bucket.model_bucket.name}/${google_storage_bucket_object.embedding_model.name}"
-}
-
-output "best_model_url" {
-  value = "https://storage.googleapis.com/${google_storage_bucket.model_bucket.name}/${google_storage_bucket_object.best_model.name}"
-}
